adb1117e193dd37ffecf37232fb877d61403b475
DovSnier <1305314861@qq.com>
2016-07-16 11:59:57 +0800

    test Scroll 滚动功能的加入;

--- app/src/main/java/com/dvsnier/testScroll/TestScrollActivity.java
+++ app/src/main/java/com/dvsnier/testScroll/TestScrollActivity.java
@@ -1,14 +1,19 @@
 package com.dvsnier.testScroll;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dvsnier.R;
 
@@ -18,6 +23,7 @@ import butterknife.OnClick;
 
 public class TestScrollActivity extends AppCompatActivity {
 
+    protected static final String TAG = TestScrollActivity.class.getSimpleName();
     @Bind(R.id.scroll_container)
     LinearLayout scrollContainer;
     @Bind(R.id.test_scroll)
@@ -28,6 +34,42 @@ public class TestScrollActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_scroll);
         ButterKnife.bind(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            scrollContainer.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+                @Override
+                public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                        v.setScrollIndicators(View.SCROLL_INDICATOR_TOP);
+//                    }
+                    if (v instanceof LinearLayout) {
+                        ViewParent parent = v.getParent();
+                        LinearLayout linearLayout = (LinearLayout) v;
+                        final int childCount = linearLayout.getChildCount();
+                        View childAtLast = linearLayout.getChildAt(childCount - 1);
+                        if (childCount > 0 && null != childAtLast) {
+                            int componentHight = childAtLast.getBottom() - childAtLast.getTop();
+                            int componentWidth = childAtLast.getRight() - childAtLast.getLeft();
+                            if (parent instanceof ScrollView) {
+                                ScrollView scrollView = (ScrollView) parent;
+                                int distance = childCount * componentHight;
+                                scrollView.scrollTo(0, distance);
+                                String msg = "total scroll distance:" + distance + "\tcomponentWidth: " + componentWidth + " & componentHight: " + componentHight;
+                                Log.d(TAG, msg);
+                                String toast = "total scroll distance:" + distance + "\ncomponentWidth: " + componentWidth + "\n componentHight: " + componentHight;
+                                Toast.makeText(TestScrollActivity.this, toast, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                }
+            });
+        } else {
+//            scrollContainer.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+//                @Override
+//                public void onGlobalLayout() {
+//
+//                }
+//            });
+        }
     }
 
     protected TextView obtainView() {
