4e9c0e8fac67805672fce20224f2356b332ac602
lizw <lizw@ichsy.com>
2016-07-28 14:09:11 +0800

    the test glide local 和 remote 功能的加入

--- app/src/main/java/com/dvsnier/testImage/TestImageActivity.java
+++ app/src/main/java/com/dvsnier/testImage/TestImageActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -21,18 +22,20 @@ public class TestImageActivity extends AppCompatActivity {
 
     @Bind(R.id.test_image)
     ImageView testImage;
-    private String url;
+    @Bind(R.id.image_url)
+    TextView imageUrl;
+    @Bind(R.id.image_local)
+    TextView imageLocal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_image);
         ButterKnife.bind(this);
-        initializedView();
     }
 
-    protected void initializedView() {
-        url = getString(R.string.image_url);
+    protected void loadImage(String url) {
+        if (null == url) return;
         Glide.with(this).load(url).crossFade().diskCacheStrategy(DiskCacheStrategy.SOURCE).listener(new RequestListener<String, GlideDrawable>() {
             @Override
             public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
@@ -47,8 +50,21 @@ public class TestImageActivity extends AppCompatActivity {
         }).placeholder(R.mipmap.ic_launcher).error(R.mipmap.ic_launcher).into(testImage);
     }
 
-    @OnClick(R.id.test_image)
+    @OnClick({R.id.image_url, R.id.image_local, R.id.test_image})
     public void onClick(View view) {
-        Toast.makeText(TestImageActivity.this, "the current test url is " + url, Toast.LENGTH_SHORT).show();
+        String url = null;
+        switch (view.getId()) {
+            case R.id.image_url:
+                url = getString(R.string.image_url);
+                loadImage(url);
+                break;
+            case R.id.image_local:
+                url = "file:///android_asset/twqrs.jpg";
+                loadImage(url);
+                break;
+            case R.id.test_image:
+                Toast.makeText(TestImageActivity.this, "the current test url is " + url, Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
