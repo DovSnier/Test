c72373ede0b8500b0767972d8142d32d8e1c4d5c
DovSnier <dovsnier@qq.com>
2019-06-24 17:29:54 +0800

    to tol that glide optimized

--- research/tpl/src/main/java/com/dvsnier/test/tpl/image/TestImageActivity.java
+++ research/tpl/src/main/java/com/dvsnier/test/tpl/image/TestImageActivity.java
@@ -1,4 +1,4 @@
-package com.dvsnier.testImage;
+package com.dvsnier.test.tpl.image;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +12,8 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
-import com.dvsnier.R;
+import com.dvsnier.test.tpl.R;
+import com.dvsnier.test.tpl.R2;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,11 +21,11 @@ import butterknife.OnClick;
 
 public class TestImageActivity extends AppCompatActivity {
 
-    @BindView(R.id.test_image)
+    @BindView(R2.id.test_image)
     ImageView testImage;
-    @BindView(R.id.image_url)
+    @BindView(R2.id.image_url)
     TextView imageUrl;
-    @BindView(R.id.image_local)
+    @BindView(R2.id.image_local)
     TextView imageLocal;
     private String url;
 
@@ -51,20 +52,17 @@ public class TestImageActivity extends AppCompatActivity {
         }).placeholder(R.mipmap.ic_launcher).error(R.mipmap.ic_launcher).into(testImage);
     }
 
-    @OnClick({R.id.image_url, R.id.image_local, R.id.test_image})
+    @OnClick({R2.id.image_url, R2.id.image_local, R2.id.test_image})
     public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.image_url:
-                url = getString(R.string.image_url);
-                loadImage(url);
-                break;
-            case R.id.image_local:
-                url = "file:///android_asset/twqrs.jpg";
-                loadImage(url);
-                break;
-            case R.id.test_image:
-                Toast.makeText(TestImageActivity.this, "the current test url is " + url, Toast.LENGTH_SHORT).show();
-                break;
+        int viewId = view.getId();
+        if (viewId == R.id.image_url) {
+            url = getString(R.string.image_url);
+            loadImage(url);
+        } else if (viewId == R.id.image_local) {
+            url = "file:///android_asset/image/twqrs.jpg";
+            loadImage(url);
+        } else if (viewId == R.id.test_image) {
+            Toast.makeText(TestImageActivity.this, "the current test url is " + url, Toast.LENGTH_SHORT).show();
         }
     }
 }
