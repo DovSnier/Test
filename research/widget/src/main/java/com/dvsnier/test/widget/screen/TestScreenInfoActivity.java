f33ebd348fd1e10c068dd19a66abff5dc4eaedf8
lizhenwei <zhenwei-li@qq.com>
2020-02-18 16:14:00 +0800

    to add screen concept

--- research/widget/src/main/java/com/dvsnier/test/widget/screen/TestScreenInfoActivity.java
+++ research/widget/src/main/java/com/dvsnier/test/widget/screen/TestScreenInfoActivity.java
@@ -28,6 +28,8 @@ public class TestScreenInfoActivity extends BaseActivity<TestScreenInfoPresenter
 
     @BindView(R2.id.content)
     TextView content;
+    @BindView(R2.id.tv_concept)
+    TextView tvConcept;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -65,22 +67,25 @@ public class TestScreenInfoActivity extends BaseActivity<TestScreenInfoPresenter
     @Override
     public void initData() {
         StringBuilder stringBuilder = new StringBuilder();
+        final String TAG_FORMAT = "%s: %s";
         DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        onLog("计算公式: ");
+        stringBuilder.append("计算公式: ")
+                .append("\n");
         Log.w("density - 计算公式:", "1 px = 1 dp * density, densityDpi = 160 * density");
-        String TAG_FORMAT = "%s: %s";
         stringBuilder.append(String.format(TAG_FORMAT, "density - 计算公式", "1 px = 1 dp * density, densityDpi = 160 * density"))
                 .append("\n");
         Log.w("density - 计算公式:", "(dpi | ppi * inch)^2 = (w^2 + h^2)");
         stringBuilder.append(String.format(TAG_FORMAT, "density - 计算公式", "(dpi | ppi * inch)^2 = (w^2 + h^2)"))
                 .append("\n");
-        Log.w("density - densityDpi", String.valueOf(displayMetrics.densityDpi));
-        stringBuilder.append(String.format(TAG_FORMAT, "density - densityDpi", String.valueOf(displayMetrics.densityDpi)))
+        Log.w("density - densityDpi", String.valueOf(displayMetrics.densityDpi) + " px/inch");
+        stringBuilder.append(String.format(TAG_FORMAT, "density - densityDpi", String.valueOf(displayMetrics.densityDpi) + " px/inch"))
                 .append("\n");
-        Log.w("density - xdpi", String.valueOf(displayMetrics.xdpi));
-        stringBuilder.append(String.format(TAG_FORMAT, "density - xdpi", String.valueOf(displayMetrics.xdpi)))
+        Log.w("density - xdpi", String.valueOf(displayMetrics.xdpi) + " px/inch");
+        stringBuilder.append(String.format(TAG_FORMAT, "density - xdpi", String.valueOf(displayMetrics.xdpi) + " px/inch"))
                 .append("\n");
-        Log.w("density - ydpi", String.valueOf(displayMetrics.ydpi));
-        stringBuilder.append(String.format(TAG_FORMAT, "density - ydpi", String.valueOf(displayMetrics.ydpi)))
+        Log.w("density - ydpi", String.valueOf(displayMetrics.ydpi) + " px/inch");
+        stringBuilder.append(String.format(TAG_FORMAT, "density - ydpi", String.valueOf(displayMetrics.ydpi) + " px/inch"))
                 .append("\n");
         Log.w("density - density", String.valueOf(displayMetrics.density));
         stringBuilder.append(String.format(TAG_FORMAT, "density - density", String.valueOf(displayMetrics.density)))
@@ -108,5 +113,29 @@ public class TestScreenInfoActivity extends BaseActivity<TestScreenInfoPresenter
                 .append("\n");
 
         content.setText(stringBuilder.toString());
+
+        //noinspection StringBufferReplaceableByString
+        StringBuilder stringBuilderWithConcept = new StringBuilder();
+        stringBuilderWithConcept.append("基本概念: ")
+                .append("\n");
+        stringBuilderWithConcept.append(String.format(TAG_FORMAT, "dip", "density independent pixels, 设备无关像素;"))
+                .append("\n");
+        stringBuilderWithConcept.append(String.format(TAG_FORMAT, "dp", "dp == dip;"))
+                .append("\n");
+        stringBuilderWithConcept.append(String.format(TAG_FORMAT, "px", "像素;"))
+                .append("\n");
+        stringBuilderWithConcept.append(String.format(TAG_FORMAT, "densityDpi", "densityDpi == dpi;"))
+                .append("\n");
+        stringBuilderWithConcept.append(String.format(TAG_FORMAT, "dpi", "dots per inch, 像素密度, 160 px/inch == 1 dip;"))
+                .append("\n");
+        stringBuilderWithConcept.append(String.format(TAG_FORMAT, "density", "密度, 其实是 DPI / (160像素/英寸) 后得到的值, 无单位;"))
+                .append("\n");
+        stringBuilderWithConcept.append(String.format(TAG_FORMAT, "分辨率", "横纵2个方向的像素点的数量;"))
+                .append("\n");
+        stringBuilderWithConcept.append(String.format(TAG_FORMAT, "屏幕尺寸", "屏幕对角线的长度;"))
+                .append("\n");
+        stringBuilderWithConcept.append(String.format(TAG_FORMAT, "屏幕比例", "只确定了对角线长, 2边长度还不一定;"))
+                .append("\n");
+        tvConcept.setText(stringBuilderWithConcept.toString());
     }
 }
