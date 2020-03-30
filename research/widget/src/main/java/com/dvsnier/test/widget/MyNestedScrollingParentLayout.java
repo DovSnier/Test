9c1352b446b4922369ba70feaaa6d39f141020e1
DovSnier <dovsnier@qq.com>
2020-03-30 17:38:03 +0800

    嵌套滚动 ScrollingParent

--- research/widget/src/main/java/com/dvsnier/test/widget/MyNestedScrollingParentLayout.java
+++ research/widget/src/main/java/com/dvsnier/test/widget/MyNestedScrollingParentLayout.java
@@ -62,22 +62,93 @@ public class MyNestedScrollingParentLayout extends LinearLayout implements Neste
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-        onLog(String.format("onNestedScroll(target: %s, dxConsumed: %s, dyConsumed: %s, dxUnconsumed: %s, dyUnconsumed: %s)", target.getId(), dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed));
-//        final int oldScrollY = getScrollY();
-//        scrollBy(0, dyUnconsumed);
-//        final int myConsumed = getScrollY() - oldScrollY;
-//        final int myUnconsumed = dyUnconsumed - myConsumed;
-//        super.onNestedScroll(target, 0, myConsumed, 0, myUnconsumed);
-        super.onNestedScroll(target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+        if (titleBarHeight > 0) {
+            // Child View 滚动完成, Parent View 继续滚动Child View 剩下尚未滚动的部分
+            if (dyUnconsumed < 0) {
+                int dyConsumedAlready = 0;
+                int dyUnConsumedAlready = 0;
+                int startScrollY = getScrollY();
+                if (getScrollY() > 0) {
+                    if (Math.abs(dyUnconsumed) >= titleBarHeight) {
+                        scrollBy(0, titleBarHeight);
+                    } else {
+                        scrollBy(0, dyUnconsumed);
+                    }
+                    dyConsumedAlready = getScrollY() - startScrollY;
+                    dyUnConsumedAlready = dyUnconsumed - dyConsumedAlready;
+                    onLog(String.format("onNestedScroll(target: %s, dxConsumed: %s, dyConsumed: %s, dxUnconsumed: %s, dyUnconsumed: %s, scrollY: %s)", target.getId(), dxConsumed, dyConsumedAlready, dxUnconsumed, dyUnConsumedAlready, getScrollY()));
+                    super.onNestedScroll(target, dxConsumed, dyConsumedAlready, dxUnconsumed, dyUnConsumedAlready);
+                } else {
+                    if (getChildCount() > 1) {
+                        final View child = getChildAt(0);
+                        if (child instanceof TextView) {
+                            scrollBy(0, child.getTop());
+                            dyConsumedAlready = getScrollY() - startScrollY;
+                            dyUnConsumedAlready = dyUnconsumed - dyConsumedAlready;
+                            onLog(String.format("onNestedScroll(target: %s, dxConsumed: %s, dyConsumed: %s, dxUnconsumed: %s, dyUnconsumed: %s, scrollY: %s)", target.getId(), dxConsumed, dyConsumedAlready, dxUnconsumed, dyUnConsumedAlready, getScrollY()));
+                            super.onNestedScroll(target, dxConsumed, dyConsumedAlready, dxUnconsumed, dyUnConsumedAlready);
+                        } else {
+                            onLog(String.format("onNestedScroll(target: %s, dxConsumed: %s, dyConsumed: %s, dxUnconsumed: %s, dyUnconsumed: %s, scrollY: %s)", target.getId(), dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, getScrollY()));
+                            super.onNestedScroll(target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+                        }
+                    } else {
+                        onLog(String.format("onNestedScroll(target: %s, dxConsumed: %s, dyConsumed: %s, dxUnconsumed: %s, dyUnconsumed: %s, scrollY: %s)", target.getId(), dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, getScrollY()));
+                        super.onNestedScroll(target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+                    }
+                }
+            } else {
+                onLog(String.format("onNestedScroll(target: %s, dxConsumed: %s, dyConsumed: %s, dxUnconsumed: %s, dyUnconsumed: %s, scrollY: %s)", target.getId(), dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, getScrollY()));
+                super.onNestedScroll(target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+            }
+        }
     }
 
+    /**
+     * Delta: Up, View: ↓, G: Up
+     * Delta: Down, View: ↑, G: Down
+     *
+     * @param target
+     * @param dx
+     * @param dy
+     * @param consumed
+     */
     @Override
     public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed) {
-        onLog(String.format("onNestedPreScroll(target: %s, dx: %s, dy: %s, consumed: %s)", target.getId(), dx, dy, consumed[0] + "," + consumed[1]));
+        if (titleBarHeight > 0) {
+            if (dy > 0) {
+                // Parent View 和Child View 同步滚动
+                // Parent View 滚动到给定值,不再同步滚动
+                // Child View 持续性滚动
+                if (Math.abs(getScrollY()) >= titleBarHeight) {
+                    consumed[1] = 0;
+                } else {
+                    scrollBy(0, dy);
+                    consumed[1] = dy;
+                }
+            } else {
+                // Child View 先滚动, Parent View 后续滚动 Child View 未滚动完的量
+            }
+        }
 //        if (titleBarHeight > 0) {
-//            scrollBy(0, -dy);
-//            consumed[1] = dy;
+//            if (dy > 0) { // dy > 0, View: ↓, G: Up
+//                if (getScrollY() > titleBarHeight) {
+//                    consumed[1] = 0;
+//                } else {
+//                    scrollBy(0, dy);
+//                    consumed[1] = dy;
+//                }
+//            } else if (dy < 0) { // dy < 0, View: ↑, G: Down
+//                if (getScrollY() > titleBarHeight) {
+//                    consumed[1] = 0;
+//                } else {
+//                    scrollBy(0, dy);
+//                    consumed[1] = dy;
+//                }
+//            } else {
+//                consumed[1] = 0;
+//            }
 //        }
+        onLog(String.format("onNestedPreScroll(target: %s, dx: %s, dy: %s, consumed: %s, scrollY: %s)", target.getId(), dx, dy, consumed[0] + "," + consumed[1], getScrollY()));
         super.onNestedPreScroll(target, dx, dy, consumed);
     }
 
