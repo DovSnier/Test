f597fb735654116ab51bf87f208b0c7520759f0f
DovSnier <dovsnier@qq.com>
2020-04-17 16:53:05 +0800

    添加富文本属性

--- research/widget/src/main/java/com/dvsnier/test/widget/CompactTextView.java
+++ research/widget/src/main/java/com/dvsnier/test/widget/CompactTextView.java
@@ -2,6 +2,7 @@ package com.dvsnier.test.widget;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
@@ -24,6 +25,8 @@ public class CompactTextView extends View implements IView {
     protected int heightLightColor;
     protected int maxLines;
     protected String ellipseSuffix;
+    protected CharSequence content;
+    protected boolean richTextClickable;
 
     public CompactTextView(Context context) {
         super(context);
@@ -61,15 +64,113 @@ public class CompactTextView extends View implements IView {
             heightLightColor = typedArray.getColor(R.styleable.CompactTextView_heightLightColor, 0);
             maxLines = typedArray.getInt(R.styleable.CompactTextView_maxLines, 0);
             ellipseSuffix = typedArray.getString(R.styleable.CompactTextView_ellipseSuffix);
+            content = typedArray.getString(R.styleable.CompactTextView_content);
+            richTextClickable = typedArray.getBoolean(R.styleable.CompactTextView_richTextClickable, true);
             typedArray.recycle();
         }
     }
 
     public void initView() {
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
     }
 
     @Override
     public void onDestroy() {
 
+    }
+
+    public int getMaxWidth() {
+        return maxWidth;
+    }
+
+    public void setMaxWidth(int maxWidth) {
+        this.maxWidth = maxWidth;
+    }
+
+    public int getMaxHeight() {
+        return maxHeight;
+    }
+
+    public void setMaxHeight(int maxHeight) {
+        this.maxHeight = maxHeight;
+    }
+
+    public int getTextSize() {
+        return textSize;
+    }
+
+    public void setTextSize(int textSize) {
+        this.textSize = textSize;
+    }
+
+    public int getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(int textColor) {
+        this.textColor = textColor;
+    }
+
+    public int getEllipsize() {
+        return ellipsize;
+    }
+
+    public void setEllipsize(int ellipsize) {
+        this.ellipsize = ellipsize;
+    }
+
+    public int getHeightLightColor() {
+        return heightLightColor;
+    }
+
+    public void setHeightLightColor(int heightLightColor) {
+        this.heightLightColor = heightLightColor;
+    }
+
+    public int getMaxLines() {
+        return maxLines;
+    }
+
+    public void setMaxLines(int maxLines) {
+        this.maxLines = maxLines;
+    }
+
+    public String getEllipseSuffix() {
+        return ellipseSuffix;
+    }
+
+    public void setEllipseSuffix(String ellipseSuffix) {
+        this.ellipseSuffix = ellipseSuffix;
+    }
+
+    public CharSequence getContent() {
+        return content;
+    }
+
+    public void setContent(CharSequence content) {
+        this.content = content;
+    }
+
+    public boolean isRichTextClickable() {
+        return richTextClickable;
+    }
+
+    public void setRichTextClickable(boolean richTextClickable) {
+        this.richTextClickable = richTextClickable;
     }
 }
