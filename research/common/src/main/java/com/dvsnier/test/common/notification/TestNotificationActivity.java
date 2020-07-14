ec0b989ce767ac1b856873fe796c97a3d1b158cf
DovSnier <dovsnier@qq.com>
2020-07-14 14:41:43 +0800

    Android 异常本地通知

--- research/common/src/main/java/com/dvsnier/test/common/notification/TestNotificationActivity.java
+++ research/common/src/main/java/com/dvsnier/test/common/notification/TestNotificationActivity.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.dvsnier.base.flavor.activity.BaseActivity;
+import com.dvsnier.base.flavor.crash.TestCrashHandleActivity;
 import com.dvsnier.test.common.R;
 import com.dvsnier.test.common.R2;
 import com.dvsnier.test.common.permission.TestPermissionActivity;
@@ -102,11 +103,56 @@ public class TestNotificationActivity extends BaseActivity {
 //                .setNumber(1)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .build();
-        notificationManager.notify(identifier++, notification);
+        if (null != notificationManager) {
+            notificationManager.notify(identifier++, notification);
+        }
     }
 
     public void buildExceptionNotification() {
+        Intent intent = new Intent(this, TestCrashHandleActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        int icon = getResources().getIdentifier("ic_launcher", "mipmap", getPackageName());
+//        String title = getString(R.string.exception_title);
+        String title = getString(getResources().getIdentifier("app_name", "string", getPackageName()));
+        String channel_name = getString(R.string.channel_exception_name);
+        String channel_id = getString(R.string.channel_exception_id);
+        String group_name = getString(R.string.channel_exception_group_name);
+        String group_id = getString(R.string.channel_exception_group_id);
+        String channel_describe = getString(R.string.channel_exception_describe);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(channel_id, channel_name,
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.setDescription(channel_describe);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            NotificationChannelGroup notificationChannelGroup = new NotificationChannelGroup(group_id, group_name);
+            notificationChannel.setGroup(group_id);
+
+            if (null != notificationManager) {
+                notificationManager.createNotificationChannelGroup(notificationChannelGroup);
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+        }
+        Notification notification = new NotificationCompat.Builder(this, channel_id)
+                .setSmallIcon(icon)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), icon))
+                .setAutoCancel(true)
+                .setBadgeIconType(NotificationCompat.BADGE_ICON_SMALL)
+//                .setChannelId(channel_id)
+                .setContentTitle(title)
+                .setContentText(getString(R.string.exception_content))
+                .setContentIntent(pendingIntent)
+                .setWhen(System.currentTimeMillis())
+//                .setNumber(1)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .build();
+        if (null != notificationManager) {
+            notificationManager.notify(identifier++, notification);
+        }
     }
 
     @Override
