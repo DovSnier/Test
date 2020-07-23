a0bdd326711bac0164df31c4c5eacb76eaf5904f
DovSnier <dovsnier@qq.com>
2020-07-23 12:55:43 +0800

    添加注解

---
+++ java/sample/src/main/java/com/dovsnier/java/sample/annotation/Type.java
@@ -0,0 +1,29 @@
+package com.dovsnier.java.sample.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Type
+ * Created by dovsnier on 2020/7/23.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE,
+        ElementType.FIELD,
+        ElementType.METHOD,
+        ElementType.PARAMETER,
+        ElementType.CONSTRUCTOR,
+        ElementType.LOCAL_VARIABLE,
+        ElementType.TYPE_PARAMETER,
+        ElementType.TYPE_USE,
+        ElementType.PACKAGE})
+@Inherited
+public @interface Type {
+
+    String name() default "";
+}
